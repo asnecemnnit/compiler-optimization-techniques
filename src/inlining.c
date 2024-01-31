5b35092278a43f953fd27621eb16b9fd054768c7
Ashish Negi <asnecemnnit@gmail.com>
2024-01-31 16:22:37 +0100

    exercise for inlining

--- src/inlining.c
+++ src/inlining.c
@@ -1,4 +1,4 @@
-// loop_unrolling.cpp
+// inlining.cpp
 
 #include "bits/stdc++.h"
 
@@ -9,15 +9,10 @@ void OP_mult_by_2(int arr[], int size) {
   }
 }
 
-// Function to perform a multiply by 2 operation on an array, optimized with
-// loop unrolling
-void OP_mult_by_2_unrolled(int arr[], int size) {
-  int i;
-  for (i = 0; i < size - 1; i += 2) {
-    arr[i] = arr[i] * 2;
-    arr[i + 1] = arr[i + 1] * 2;
-  }
-  for (; i < size; ++i) {
+// Function to perform a multiply by 2 operation on an array, marked for
+// inlining
+inline void OP_mult_by_2_inlined(int arr[], int size) {
+  for (int i = 0; i < size; ++i) {
     arr[i] = arr[i] * 2;
   }
 }
@@ -27,9 +22,11 @@ int main() {
   int array1[size] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   int array2[size] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-  // Call the function
+  // Call the regular function
   OP_mult_by_2(array1, size);
-  OP_mult_by_2_unrolled(array2, size);
+
+  // Call the inlined function
+  OP_mult_by_2_inlined(array2, size);
 
   for (int i = 0; i < size; ++i) {
     assert(array1[i] == array2[i]);
